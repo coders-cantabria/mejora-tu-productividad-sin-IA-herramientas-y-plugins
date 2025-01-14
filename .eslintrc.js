ab6f297cc13b38c990192d9e1139fda81a7de587
Óscar Saro Calderón <oscarsc1997@gmail.com>
2025-01-14 09:25:50 +0100

    add husky package & basic config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,20 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
     es2021: true,
     node: true
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended' // Includes recommended TypeScript linting rules
-  ],
-  parser: '@typescript-eslint/parser', // Specifies the ESLint parser for TypeScript
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module'
   },
-  plugins: ['@typescript-eslint'], // Enables the TypeScript plugin
+  plugins: ['@typescript-eslint'],
   rules: {
-    '@typescript-eslint/no-explicit-any': 'error' // Disallows the use of the 'any' type
-  }
+    '@typescript-eslint/no-explicit-any': 'error',
+    'no-console': 'error'
+  },
+  ignorePatterns: ['/dist', '/.angular', '/src/app/api', '/.history', '/documentation', '/coverage']
 };
