e5cc018e497b9e8fc2272dff2681a28d28d0e485
Óscar Saro Calderón <oscarsc1997@gmail.com>
2025-01-24 12:52:50 +0100

    add unused-import eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,10 +11,20 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module'
   },
-  plugins: ['@typescript-eslint', 'html'],
+  plugins: ['@typescript-eslint', 'html', 'unused-imports'],
   rules: {
     '@typescript-eslint/no-explicit-any': 'error',
-    'no-console': 'warn'
+    'no-console': 'warn',
+    'unused-imports/no-unused-imports': 'error', // Remove unused imports
+    'unused-imports/no-unused-vars': [
+      'warn', // Warn for unused variables
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_'
+      }
+    ]
   },
   ignorePatterns: ['/dist', '/.angular', '/src/app/api', '/.history', '/documentation', '/coverage']
 };
