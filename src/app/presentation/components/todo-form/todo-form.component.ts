65f9da2bd325b0d62d3067483b381e0f3ca7619a
Óscar Saro Calderón <oscarsc1997@gmail.com>
2025-01-27 09:07:45 +0100

    add TODO form component

--- src/app/presentation/components/todo-form/todo-form.component.ts
+++ src/app/presentation/components/todo-form/todo-form.component.ts
@@ -1,9 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { TodoManagerService } from '../../../services/todo-manager.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-todo-form',
-  imports: [],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, CommonModule],
   templateUrl: './todo-form.component.html',
   styleUrl: './todo-form.component.scss'
 })
-export class TodoFormComponent {}
+export class TodoFormComponent {
+  #formBuilder = inject(FormBuilder);
+  #todosService = inject(TodoManagerService);
+
+  todoForm: FormGroup = this.#formBuilder.group({
+    title: ['', [Validators.required, Validators.maxLength(100)]],
+    description: ['', [Validators.maxLength(5000)]]
+  });
+
+  submitForm() {
+    this.#todosService.addTodo({
+      ...this.todoForm.value,
+      date: new Date()
+    });
+
+    this.resetForm();
+  }
+
+  resetForm() {
+    this.todoForm.reset(
+      {
+        title: ''
+      },
+      { emitEvent: true }
+    );
+  }
+}
