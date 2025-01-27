7ffe2147a73152149def028accec592e6441fd14
Óscar Saro Calderón <oscarsc1997@gmail.com>
2025-01-27 09:01:50 +0100

    add TODO component

--- src/app/presentation/components/todo/todo.component.ts
+++ src/app/presentation/components/todo/todo.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
+import { TodoInterface } from '../../types/todo.interface';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { DatePipe } from '@angular/common';
+import { TodoManagerService } from '../../../services/todo-manager.service';
 
 @Component({
   selector: 'app-todo',
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, DatePipe],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.scss'
 })
-export class TodoComponent {}
+export class TodoComponent {
+  #todosService = inject(TodoManagerService);
+
+  todo = input.required<TodoInterface>();
+
+  removeTodo() {
+    this.#todosService.removeTodo(this.todo().id);
+  }
+}
