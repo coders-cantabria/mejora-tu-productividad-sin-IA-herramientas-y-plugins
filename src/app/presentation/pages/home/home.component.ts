761872a5cb5976d1afb4d5526abffcfc903ec9b6
Óscar Saro Calderón <oscarsc1997@gmail.com>
2025-01-27 09:07:59 +0100

    add home component

--- src/app/presentation/pages/home/home.component.ts
+++ src/app/presentation/pages/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { TodoListComponent } from '../../components/todo-list/todo-list.component';
+import { TodoFormComponent } from '../../components/todo-form/todo-form.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [TodoListComponent, TodoFormComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
