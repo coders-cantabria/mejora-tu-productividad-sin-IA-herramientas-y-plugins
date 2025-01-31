f183913e85813b1077152089a303b3c76921da56
Óscar Saro Calderón <oscarsc1997@gmail.com>
2025-01-31 09:52:03 +0100

    add fetch todos testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,44 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { provideHttpClient } from '@angular/common/http';
+import SpyObj = jasmine.SpyObj;
+import { TodoManagerService } from './services/todo-manager.service';
+import createSpyObj = jasmine.createSpyObj;
+import { of } from 'rxjs';
+import { todoMocks } from './presentation/mocks/todo.mocks';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let todoManagerServiceSpyObj: SpyObj<TodoManagerService>;
+
   beforeEach(async () => {
+    todoManagerServiceSpyObj = createSpyObj({
+      fetchTodos: of([todoMocks()])
+    });
+
     await TestBed.configureTestingModule({
       imports: [AppComponent],
-      providers: [provideHttpClient()]
+      providers: [
+        provideHttpClient(),
+        {
+          provide: TodoManagerService,
+          useValue: todoManagerServiceSpyObj
+        }
+      ]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
+  });
+
+  it('should call fetchTodos on ngOnInit', () => {
+    component.ngOnInit();
+
+    expect(todoManagerServiceSpyObj.fetchTodos).toHaveBeenCalled();
   });
 });
--- src/app/services/todo-manager.service.spec.ts
+++ src/app/services/todo-manager.service.spec.ts
@@ -3,15 +3,19 @@ import { TestBed } from '@angular/core/testing';
 import { TodoManagerService } from './todo-manager.service';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
+import { TodoInterface } from '../presentation/types/todo.interface';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('TodoManagerService', () => {
   let service: TodoManagerService;
+  let http: HttpTestingController;
   let currentLength: number;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [provideAnimations(), provideHttpClient()]
+      providers: [provideAnimations(), provideHttpClient(), provideHttpClientTesting()]
     });
+    http = TestBed.inject(HttpTestingController);
     service = TestBed.inject(TodoManagerService);
 
     currentLength = service.todos.length;
@@ -58,5 +62,53 @@ describe('TodoManagerService', () => {
     service.updateTodo({ ...todo, title: 'Test 2' });
 
     expect(service.todos[0].title).toBe('Test 2');
+  });
+
+  it('should update a todo', () => {
+    service.addTodo({
+      title: 'Test 1',
+      date: new Date()
+    });
+
+    service.addTodo({
+      title: 'Test 3',
+      date: new Date()
+    });
+
+    expect(service.todos.length).toBe(currentLength + 2);
+
+    const [todo] = service.todos;
+
+    service.updateTodo({ ...todo, title: 'Test 2' });
+
+    expect(service.todos[0].title).toBe('Test 2');
+  });
+
+  it('should fetch todos', () => {
+    const mockTodos: TodoInterface[] = [
+      { id: '1', title: 'Test Todo 1', date: new Date(), completed: false },
+      { id: '2', title: 'Test Todo 2', date: new Date(), completed: false }
+    ];
+
+    service.fetchTodos();
+
+    const req = http.expectOne('https://api.mockos.io/api/v1/todos');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockTodos);
+
+    expect(service.todos.length).toBe(mockTodos.length);
+    expect(service.todos).toEqual(mockTodos);
+  });
+
+  it('should handle error when fetching todos', () => {
+    service.loading.set(true);
+
+    service.fetchTodos();
+
+    const req = http.expectOne('https://api.mockos.io/api/v1/todos');
+    expect(req.request.method).toBe('GET');
+    req.flush('Error fetching data', { status: 500, statusText: 'Server Error' });
+
+    expect(service.loading()).toBe(false);
   });
 });
