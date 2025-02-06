403712b1d9eda890cf45923a9daa9998fbc3c71f
Óscar Saro Calderón <oscarsc1997@gmail.com>
2025-02-06 17:08:35 +0100

    fix: remove unused code

--- src/app/presentation/components/todo-list/todo-list.component.ts
+++ src/app/presentation/components/todo-list/todo-list.component.ts
@@ -2,11 +2,10 @@ import { Component, computed, inject } from '@angular/core';
 import { TodoComponent } from '../todo/todo.component';
 import { TodoManagerService } from '../../../services/todo-manager.service';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-todo-list',
-  imports: [TodoComponent, JsonPipe],
+  imports: [TodoComponent],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.scss'
 })
